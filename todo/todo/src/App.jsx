4e14e46a78e2766c5767cd23f9b34833688f258b
code-sharad <codesharad@gmail.com>
2023-08-26 20:02:55 +0530

    add feature of delete item

--- todo/todo/src/App.jsx
+++ todo/todo/src/App.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react'
 import axios, { Axios } from 'axios'
 import './App.css'
 
+import {v4 as uuid4} from 'uuid';
+
 function App() {
   const [name, setName] = useState('');
-  const [backedData,setBackedData] = useState([{}]);
+  const [backedData, setBackedData] = useState([]);
 
   let todo = [];
   function sendData() {
@@ -29,25 +31,45 @@ function App() {
       .catch(err => {
         console.log(err);
       })
-  }, backedData)
+  }, [])
 
   // backedData.map((data)=>{
   //   console.log(data['inputData'])
   // })
-  
-  
+
+  const handleAddTodo = (e) => {
+    setName(e.target.value)
+    // setBackedData(prevTodos => [...prevTodos, e.target.value]);
+    
+  }
+
+  const handleDeleteTodo = (id) => {
+    const updatedItems = backedData.filter((elem,ind) =>{
+        return ind !== id;
+    });
+    setBackedData(updatedItems);
+  }
+
+  const removeAll = () => {
+    return setBackedData([])
+  }
 
   return (
     <>
       <div>
         <label htmlFor="name">
-          Name <input onChange={(e) => setName(e.target.value)} type="text" value={name} name="name" id="" />
+          Name <input onChange={handleAddTodo} type="text" value={name} name="name" id="" />
           <button onClick={sendData}>submit</button>
         </label>
 
-          {backedData.map((data,i) => <p key={i}>{data['inputData']}</p>)}
+        {backedData.map((data, i) =>
+          <div key={i} className='drop-shadow' style={{'display':'flex','gap':'3rem','marginTop':'1rem','justifyContent':'space-evenly'}}>
+             <p className='p' >{data['inputData']}</p><button onClick={() => handleDeleteTodo(i)}>x</button>
+            </div>
+        )}
 
       </div>
+      <button onClick={removeAll}>remove all</button>
     </>
   )
 }
